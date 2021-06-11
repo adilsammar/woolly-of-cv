56c6723659cbcd8e29714c2099831070e9b84c9f
Ammar Adil <ammaradil80@gmail.com>
2021-06-11 22:39:44 +0530

    Added Documentation

--- assets/mnist/mnist/backpropagation.py
+++ assets/mnist/mnist/backpropagation.py
@@ -5,7 +5,26 @@ from torch.optim import SGD
 torch.manual_seed(1)
 
 def train(use_l1=False, lambda_l1=5e-4):
+    """ Function to return train function instance
+
+    Args:
+        use_l1 (bool, optional): Enable L1. Defaults to False.
+        lambda_l1 (float, optional): L1 Value. Defaults to 5e-4.
+    """
     def internal(model, train_loader, optimizer, dropout, device, scheduler=None):
+        """ This function is for running backpropagation
+
+        Args:
+            model (Net): Model instance to train
+            train_loader (Dataset): Dataset used in training
+            optimizer (torch.optim): Optimizer used
+            dropout (bool): Enable/Disable 
+            device (string, cuda/cpu): Device type Values Allowed - cuda/cpu
+            scheduler (Scheduler, optional): scheduler instance used for updating lr while training. Defaults to None.
+
+        Returns:
+            (float, int): Loss, Number of correct Predictions
+        """
         model.train()
         epoch_loss = 0
         correct = 0
@@ -23,16 +42,27 @@ def train(use_l1=False, lambda_l1=5e-4):
             optimizer.step()
             if scheduler:
                 scheduler.step()
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            # get the index of the max log-probability
+            pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
             epoch_loss += loss.item()
 
         return epoch_loss / len(train_loader), correct
-    
+
     return internal
 
 
 def test(model, test_loader, device):
+    """[summary]
+
+    Args:
+        model (Net): Model instance to run validation
+        test_loader (Dataset): Dataset used in validation
+        device (string, cuda/cpu): Device type Values Allowed - cuda/cpu
+
+    Returns:
+        (float, int): Loss, Number of correct Predictions
+    """
     model.eval()
     test_loss = 0
     correct = 0
@@ -40,15 +70,16 @@ def test(model, test_loader, device):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            # sum up batch loss
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            # get the index of the max log-probability
+            pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
-    
+
     return test_loss, correct
 
 
 def get_sgd_optimizer(model, lr, momentum=0, weight_decay=0):
     return SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
-    
