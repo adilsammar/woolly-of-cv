bee26c07d02a690a9fc470e6d9fed2e24f4fb68a
Ammar Adil <ammaradil80@gmail.com>
2021-06-11 23:51:29 +0530

    Add Comments

--- assets/mnist/mnist/transform.py
+++ assets/mnist/mnist/transform.py
@@ -6,19 +6,31 @@ from torchvision import transforms
 
 torch.manual_seed(1)
 
+
 def get_a_train_transform():
+    """Get transformer for training data
+
+    Returns:
+        Compose: Composed transformations
+    """
     return A.Compose([
-        A.ShiftScaleRotate(shift_limit=0.09, scale_limit=0.09, rotate_limit=7, p=0.5),
-#         A.RandomResizedCrop(height=26, width=26, scale=(0.8, 1.0), p=0.3),
+        A.ShiftScaleRotate(shift_limit=0.09, scale_limit=0.09,
+                           rotate_limit=7, p=0.5),
+        #         A.RandomResizedCrop(height=26, width=26, scale=(0.8, 1.0), p=0.3),
         A.RandomBrightnessContrast(p=0.5),
-#         A.GaussNoise(p=0.2),
-#         A.Equalize(p=0.2),
+        #         A.GaussNoise(p=0.2),
+        #         A.Equalize(p=0.2),
         A.Normalize(mean=(0.1307,), std=(0.3081,)),
         ToTensorV2(),
     ])
 
 
 def get_a_test_transform():
+    """Get transformer for test data
+
+    Returns:
+        Compose: Composed transformations
+    """
     return A.Compose([
         A.Normalize(mean=(0.1307,), std=(0.3081,)),
         ToTensorV2(),
@@ -26,6 +38,11 @@ def get_a_test_transform():
 
 
 def get_p_train_transform():
+    """Get Pytorch Transform function for train data
+
+    Returns:
+        Compose: Composed transformations
+    """
     random_rotation_degree = 5
     img_size = (28, 28)
     random_crop_percent = (0.85, 1.0)
@@ -38,8 +55,12 @@ def get_p_train_transform():
 
 
 def get_p_test_transform():
+    """Get Pytorch Transform function for test data
+
+    Returns:
+        Compose: Composed transformations
+    """
     return transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
     ])
-
