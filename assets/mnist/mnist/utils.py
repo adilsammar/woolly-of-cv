f31072fa24874f625b14ee8d29f934a5e78630eb
Ammar Adil <ammaradil80@gmail.com>
2021-06-12 00:02:38 +0530

    Added Comments

--- assets/mnist/mnist/utils.py
+++ assets/mnist/mnist/utils.py
@@ -6,32 +6,54 @@ from torchsummary import summary
 
 torch.manual_seed(1)
 
+
 def get_device() -> tuple:
+    """Get Device type
+
+    Returns:
+        tuple: Device type
+    """
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
     return (use_cuda, device)
 
 
 def print_summary(model, input_size=(1, 28, 28)):
+    """Print Model summary
+
+    Args:
+        model (Net): Model Instance
+        input_size (tuple, optional): Input size. Defaults to (1, 28, 28).
+    """
     summary(model, input_size=input_size)
-    
+
 
 def print_modal_summary(model):
-    print (f'--------------------------------------------------------')
-    print (f'| {"Name":25}\t{"Shape":15}\tParams |')
-    print (f'--------------------------------------------------------')
+    """Print Model summary
+
+    Args:
+        model (Net): Model Instance
+    """
+    print(f'--------------------------------------------------------')
+    print(f'| {"Name":25}\t{"Shape":15}\tParams |')
+    print(f'--------------------------------------------------------')
     total = 0
     for name, param in model.named_parameters():
         if param.requires_grad:
             count = np.prod(list(param.data.shape))
             total += count
-            print (f'| {name:25}\t{str(list(param.data.shape)):15}\t{count:6} |')
-    print (f'--------------------------------------------------------')
+            print(f'| {name:25}\t{str(list(param.data.shape)):15}\t{count:6} |')
+    print(f'--------------------------------------------------------')
     print(f'| {"Total":25}\t{"":15}\t{total:6} |')
     print(f'--------------------------------------------------------')
 
 
 def initialize_weights(m):
+    """Function to initialize random weights
+
+    Args:
+        m (nn.Module): Layer instance
+    """
     if isinstance(m, nn.Conv2d):
         nn.init.xavier_uniform_(m.weight.data)
         if m.bias is not None:
@@ -39,14 +61,33 @@ def initialize_weights(m):
     elif isinstance(m, nn.BatchNorm2d):
         nn.init.constant_(m.weight.data, 1)
         nn.init.constant_(m.bias.data, 0)
-        
+
 
 def load_weights_from_path(model, path):
+    """load weights from file
+
+    Args:
+        model (Net): Model instance
+        path (str): Path to weights file
+
+    Returns:
+        Net: loaded model
+    """
     model.load_state_dict(torch.load(path))
     return model
 
 
 def get_all_predictions(model, loader, device):
+    """Get All predictions for model
+
+    Args:
+        model (Net): Trained Model 
+        loader (Dataloader): instance of dataloader
+        device (str): Which device to use cuda/cpu
+
+    Returns:
+        tuple: all predicted values and their targets
+    """
     model.eval()
     all_preds = torch.tensor([]).to(device)
     all_targets = torch.tensor([]).to(device)
@@ -62,12 +103,22 @@ def get_all_predictions(model, loader, device):
             all_preds = torch.cat(
                 (all_preds, preds),
                 dim=0
-            ) 
-    
+            )
+
     return all_preds, all_targets
 
 
 def get_incorrrect_predictions(model, loader, device):
+    """Get all incorrect predictions
+
+    Args:
+        model (Net): Trained model
+        loader (DataLoader): instance of data loader
+        device (str): Which device to use cuda/cpu
+
+    Returns:
+        list: list of all incorrect predictions and their corresponding details
+    """
     model.eval()
     incorrect = []
     with torch.no_grad():
@@ -78,15 +129,26 @@ def get_incorrrect_predictions(model, loader, device):
             pred = output.argmax(dim=1)
             for d, t, p, o in zip(data, target, pred, output):
                 if p.eq(t.view_as(p)).item() == False:
-                    incorrect.append([d.cpu(), t.cpu(), p.cpu(), o[p.item()].cpu()])
-    
+                    incorrect.append(
+                        [d.cpu(), t.cpu(), p.cpu(), o[p.item()].cpu()])
+
     return incorrect
 
 
 def prepare_confusion_matrix(all_preds, all_targets, class_map):
+    """Prepare Confusion matrix
+
+    Args:
+        all_preds (list): List of all predictions
+        all_targets (list): List of all actule labels
+        class_map (dict): Class names
+
+    Returns:
+        tensor: confusion matrix for size number of classes * number of classes
+    """
     stacked = torch.stack((
-            all_targets, all_preds
-        ),
+        all_targets, all_preds
+    ),
         dim=1
     ).type(torch.int64)
 
